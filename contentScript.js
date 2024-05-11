bdfac161cd579d9e73fe11e67c98e41192605017
manish8576 <67515371+manish8576@users.noreply.github.com>
2024-05-12 00:07:49 +0530

    VER 1.0.5

--- contentScript.js
+++ contentScript.js
@@ -1,133 +1,90 @@
 console.log("content script injected")
 
-
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.from === "popup" && message.query === "clicked") {
-
         setTimeout(() => {
-
-
             var endButton = "";
             var statusOfPage = "";
-            var totalDeliveredVlaue = 0;
+            var totalDeliveredValue = 0;
             var cancelledOrder = 0;
             var returnedOrder = 0;
             var DeliveredOrder = 0;
             var totalOrder;
 
             function scrollToBottom() {
-
-
                 function scrollHandler() {
-
                     var scrollableHeight = document.body.scrollHeight;
                     if (document.getElementsByClassName("QqFHMw v0q-qo").length > 0) {
                         statusOfPage = document.getElementsByClassName("QqFHMw v0q-qo")[0].innerText;
                     }
-
                     if (statusOfPage === "Show More Orders") {
-                        document.getElementsByClassName("QqFHMw v0q-qo")[0].click();
+                        document.getElementsByClassName("QqFHMw v0q-qo")[0].click()
                         window.scrollTo(0, scrollableHeight);
                     } else {
                         window.scrollTo(0, scrollableHeight);
                     }
-
                     if (endButton != "No More Results To Display") {
                         setTimeout(checkNextScroll, 1000);
                     }
                 }
 
                 function checkNextScroll() {
-
-
                     if (document.getElementsByClassName("v0q-qo").length > 0) {
                         endButton = document.getElementsByClassName("v0q-qo")[0].innerText;
                     }
                     if (endButton != "No More Results To Display") {
-
                         window.onload = scrollHandler();
                     }
                     if (endButton == "No More Results To Display") {
-
                         console.log("Finished scrolling")
                         startAnalyzing();
                     }
-
-                    
-                    }
-
+                }
 
                 window.onload = checkNextScroll();
-                
             }
 
             function startAnalyzing() {
                 console.log("Start Analyzing")
-
-                var elementsStatusArray = Array.from(
-                    document.getElementsByClassName("g1SRZp")
-                );
-                var elementsPriceArray = Array.from(
-                    document.getElementsByClassName("col-2-12 mcVLQq")
-                );
-
+                var elementsStatusArray = Array.from(document.getElementsByClassName("g1SRZp"));
+                var elementsPriceArray = Array.from(document.getElementsByClassName("col-2-12 mcVLQq"));
                 var lenStatusArray = elementsStatusArray.length;
-                
+
                 for (var i = 0; i < lenStatusArray; i++) {
                     priceArray = elementsPriceArray[i].innerText;
                     orderStatus = elementsStatusArray[i].innerText.split(" ")[0];
                     refundStatus = elementsStatusArray[i].innerText;
 
-                    if (orderStatus == "Refund completed") {
-                        
+                    if (refundStatus == "Refund completed") {
                         returnedOrder++;
-                    }
-
-                    else if (orderStatus == "Cancelled") {
+                    } else if (orderStatus == "Cancelled") {
                         cancelledOrder++;
-                    }
-                    else if (orderStatus == "Delivered") {
-                        
+                    } else if (orderStatus == "Delivered" || refundStatus == "Refund Rejected") {
                         var price = "";
                         for (var j = 0; j < priceArray.length; j++) {
                             var elem = priceArray[j];
-
                             if (elem === "+") {
                                 break;
                             }
-
                             if (!isNaN(parseInt(elem))) {
                                 price += elem;
                             }
                         }
-
                         price = parseInt(price);
                         if (!isNaN(price)) {
                             DeliveredOrder++;
-                            totalDeliveredVlaue += price;
+                            totalDeliveredValue += price;
                         }
                     }
                 }
                 totalOrder = returnedOrder + cancelledOrder + DeliveredOrder;
                 chrome.storage.local.get("orderDetails", (resp) => {
-                    chrome.storage.local.set({ "orderDetails": [totalOrder, cancelledOrder, DeliveredOrder, returnedOrder, totalDeliveredVlaue] })
-
+                    chrome.storage.local.set({
+                        "orderDetails": [totalOrder, cancelledOrder, DeliveredOrder, returnedOrder, totalDeliveredValue]
+                    })
                 })
             }
             scrollToBottom();
-            
         }, 500);
     }
 })
-            
-
-
-
-
-
-
-
-
-
-
-
--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 window.addEventListener('DOMContentLoaded', () => {
     const mainCont = document.getElementById("mainCont");
     const clearButton = document.getElementById("clearButton");
@@ -6,73 +5,47 @@ window.addEventListener('DOMContentLoaded', () => {
     const resultList = document.getElementsByClassName("result");
 
     chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-        const tab = tabs[0]
-
-
+        const tab = tabs[0];
         if (tab.url.includes('flipkart') && tab.url.includes('orders')) {
-            const button = document.createElement("button")
-            button.setAttribute("class", "analyze_btn")
-
-            button.innerText = "Analyze"
-
-
+            const button = document.createElement("button");
+            button.setAttribute("class", "analyze_btn");
+            button.innerText = "Analyze";
             button.addEventListener("click", () => {
-
-
                 chrome.tabs.sendMessage(
                     tabs[0].id,
                     { from: "popup", query: "clicked" }
                 );
                 document.getElementById("requistie").style.display = "block";
-                window.close()
-
-            })
-
-            buttonCont.appendChild(button)
-
+                window.close();
+            });
+            buttonCont.appendChild(button);
         }
         else {
-            const errorMsg = document.createElement("h1")
-            errorMsg.innerText = 'First please Open flipkart orders section'
-            errorMsg.setAttribute("id", "errorMsg")
-            buttonCont.appendChild(errorMsg)
+            const errorMsg = document.createElement("h1");
+            errorMsg.innerText = 'First please Open flipkart orders section';
+            errorMsg.setAttribute("id", "errorMsg");
+            buttonCont.appendChild(errorMsg);
             document.getElementById("subCont").style.display = "none";
-
         }
     });
 
-
-
-
-    function showResults(){
-        
-    }
-
     chrome.storage.local.get("orderDetails", (resp) => {
-
         if (resp.orderDetails && resp.orderDetails.length > 0) {
-            i = 0
+            let i = 0;
             resp.orderDetails.forEach(data => {
-                resultList[i].innerText += data
-                i++
-            })
-
-            const ClearButton = document.createElement("button")
-            ClearButton.innerText = "Clear Data"
-            ClearButton.setAttribute("id", "clearBtn")
+                resultList[i].innerText += data;
+                i++;
+            });
+            const ClearButton = document.createElement("button");
+            ClearButton.innerText = "Clear Data";
+            ClearButton.setAttribute("id", "clearBtn");
             ClearButton.addEventListener("click", () => {
-                chrome.storage.local.remove("orderDetails")
-                window.close()
-            })
-            clearButton.appendChild(ClearButton)
+                chrome.storage.local.remove("orderDetails");
+                window.close();
+            });
+            clearButton.appendChild(ClearButton);
             document.getElementById("subCont").style.display = "block";
             document.getElementById("requistie").style.display = "none";
-
         }
-
-
-    })
-
-
-})
-
+    });
+});
