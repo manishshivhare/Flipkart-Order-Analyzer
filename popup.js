df8daf4b4f785b24d8af6088c0ef4cedc49d355f
manish8576 <67515371+manish8576@users.noreply.github.com>
2024-05-29 19:04:30 +0530

    VER 1.1.0

--- popup.js
+++ popup.js
@@ -8,7 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
         const tab = tabs[0];
         if (tab.url.includes('flipkart') && tab.url.includes('orders')) {
             const button = document.createElement("button");
-            button.setAttribute("class", "analyze_btn");
+            button.setAttribute("class", "analyze_btn " + "button-17");
             button.innerText = "Analyze";
             buttonCont.appendChild(button);
             button.addEventListener("click", () => {
@@ -36,17 +36,20 @@ window.addEventListener('DOMContentLoaded', () => {
         if (orderDetails.length > 0) {
             let i = 0;
             orderDetails.forEach(data => {
-                resultList[i].innerText += data;
+                resultList[i].innerText = data;
                 i++;
             });
-            const ClearButton = document.createElement("button");
-            ClearButton.innerText = "Clear Data";
-            ClearButton.setAttribute("id", "clearBtn");
-            ClearButton.addEventListener("click", () => {
-                chrome.storage.local.remove("orderDetails");
-                window.close();
-            });
-            clearButton.appendChild(ClearButton);
+            if (!document.getElementById("clearBtn")) {
+                const ClearButton = document.createElement("button");
+                ClearButton.innerText = "Clear Data";
+                ClearButton.setAttribute("id", "clearBtn");
+                ClearButton.setAttribute("class", "button-17");
+                ClearButton.addEventListener("click", () => {
+                    chrome.storage.local.remove("orderDetails");
+                    window.close();
+                });
+                clearButton.appendChild(ClearButton);
+            }
             document.getElementById("subCont").style.display = "block";
             document.getElementById("requistie").style.display = "none";
         }
